8f6e6fbbe987d8097eb25e00228269873aa2fa04
Sue-cod <snozic892@gmail.com>
2021-03-15 18:58:15 +0100

    Drugi zadatak

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+    
+console.log ("John", "23");
+console.log ("Michael", "43");
+
+
+}
+functionPrintInfo()
+// Write code above this line
+
+module.exports = functionPrintInfo;
