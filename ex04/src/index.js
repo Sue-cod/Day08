71e15b37686384142c21afc83f8ea3f028c586ca
Sue-cod <snozic892@gmail.com>
2021-03-15 19:47:07 +0100

    Cetvrti zadatak

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+function localScope() {
+myVariable = "I am here!";
+}
+
+if (typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable)
+} else {
+    console.log('outside localScope UNDEFINED!!!')
+}
+
+module.exports = localScope;
