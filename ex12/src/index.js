b2932ff5b4c7f274e9a02a4bc924f69a0ca965e5
Sue-cod <snozic892@gmail.com>
2021-03-16 02:07:04 +0100

    Dvanaesti zadatak

---
+++ ex12/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function randomInteger() {
+Math.floor(Math.random()) * 30;
+return Math.floor(Math.random());
+}
+randomInteger()
+// Only change code above this line
+console.log(randomInteger());
+module.exports = randomInteger;
